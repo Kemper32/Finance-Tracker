582fa3a129446bc165d0308027fa6af0c1efe5e8
Chris <Kemper1324@gmail.com>
2017-12-29 17:55:12 +0000

    pushing to make sure I have made backups

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,17 @@
+class StocksController < ApplicationController
+ 
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render partial: 'users/result'
+      else
+        flash[:danger] = "You have entered an incorrect symbol"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "You have entered an empty string"
+      redirect_to my_portfolio_path
+    end
+  end
+end
